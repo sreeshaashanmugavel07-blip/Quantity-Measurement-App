053aae3eb8d2b32dee56f07eb2283174fe83bc9b
sreeshaashanmugavel07-blip <sreeshaashanmugavel07@gmail.com>
2026-04-17 16:14:39 +0530

    UC3: Generic Quantity with conversion

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -2,49 +2,50 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-    public static class Feet {
-        private final double value;
+    // Enum for units
+    enum Unit {
+        FEET(1.0),
+        INCHES(1.0 / 12);
 
-        public Feet(double value) {
-            this.value = value;
+        private final double factor;
+
+        Unit(double factor) {
+            this.factor = factor;
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-
-            Feet feet = (Feet) obj;
-            return Double.compare(feet.value, value) == 0;
+        public double toFeet(double value) {
+            return value * factor;
         }
     }
 
-    // UC2: Inches class
-    public static class Inches {
+    // Generic Quantity class
+    public static class Quantity {
         private final double value;
+        private final Unit unit;
 
-        public Inches(double value) {
+        public Quantity(double value, Unit unit) {
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            return unit.toFeet(value);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
+            if (!(obj instanceof Quantity)) return false;
 
-            Inches inches = (Inches) obj;
-            return Double.compare(inches.value, value) == 0;
+            Quantity q = (Quantity) obj;
+            return Double.compare(this.toFeet(), q.toFeet()) == 0;
         }
     }
 
     public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+        Quantity q1 = new Quantity(1.0, Unit.FEET);
+        Quantity q2 = new Quantity(12.0, Unit.INCHES);
 
-        Inches i1 = new Inches(1.0);
-        Inches i2 = new Inches(1.0);
-
-        System.out.println("Feet Equal: " + f1.equals(f2));
-        System.out.println("Inches Equal: " + i1.equals(i2));
+        System.out.println("Equal: " + q1.equals(q2));
     }
 }
