4d16154724b0c3281f150487186c3e3f42614976
sreeshaashanmugavel07-blip <sreeshaashanmugavel07@gmail.com>
2026-04-17 16:13:41 +0530

    UC2: Inches equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -18,4 +18,33 @@ public class QuantityMeasurementApp {
             return Double.compare(feet.value, value) == 0;
         }
     }
+
+    // UC2: Inches class
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Feet Equal: " + f1.equals(f2));
+        System.out.println("Inches Equal: " + i1.equals(i2));
+    }
 }
