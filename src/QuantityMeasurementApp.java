f751f9801655a3ae1e758c52e643e7c066b1fddc
Sreeshaa Shanmugavel <sreeshaashanmugavel07@gmail.com>
2026-04-17 16:17:34 +0530

    Update QuantityMeasurementApp.java

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -2,55 +2,28 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-    enum Unit {
-        FEET(1.0),
-        INCHES(1.0 / 12),
-        YARDS(3.0),
-        CENTIMETERS(0.0328084);
-
-        private final double factor;
-
-        Unit(double factor) {
-            this.factor = factor;
-        }
-
-        public double toFeet(double value) {
-            return value * factor;
-        }
-    }
-
-    public static class Quantity {
+    // UC1: Feet class
+    public static class Feet {
         private final double value;
-        private final Unit unit;
 
-        public Quantity(double value, Unit unit) {
+        public Feet(double value) {
             this.value = value;
-            this.unit = unit;
-        }
-
-        private double toFeet() {
-            return unit.toFeet(value);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (!(obj instanceof Quantity)) return false;
+            if (obj == null || getClass() != obj.getClass()) return false;
 
-            Quantity q = (Quantity) obj;
-            return Double.compare(this.toFeet(), q.toFeet()) == 0;
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
         }
     }
 
     public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
 
-        Quantity q1 = new Quantity(1.0, Unit.YARDS);
-        Quantity q2 = new Quantity(3.0, Unit.FEET);
-
-        Quantity q3 = new Quantity(1.0, Unit.CENTIMETERS);
-        Quantity q4 = new Quantity(0.393701, Unit.INCHES);
-
-        System.out.println("Yard to Feet Equal: " + q1.equals(q2));
-        System.out.println("CM to Inch Equal: " + q3.equals(q4));
+        System.out.println("Feet Equal: " + f1.equals(f2));
     }
 }
