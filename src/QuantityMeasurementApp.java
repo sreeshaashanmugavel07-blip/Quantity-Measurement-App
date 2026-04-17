ca68f3d6da2823c0fe16337b4561951a83100679
sreeshaashanmugavel07-blip <sreeshaashanmugavel07@gmail.com>
2026-04-17 16:04:45 +0530

    UC1: Feet equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,2 +1,21 @@
+package com.apps.quantitymeasurement;
+
 public class QuantityMeasurementApp {
+
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
+        }
+    }
 }
